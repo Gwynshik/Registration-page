9dc0f1a12cbbcefce2d4fd825c45cb481cbb496c
Gwynshik <gwynshik@gmail.com>
2022-08-12 00:22:03 +0300

    Add base html

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-
+    <div>
+        <div>фото</div>
+        <div>
+            <div>
+                <button type="button"><p>стрелка влево</p></button>
+                <div>
+                    <p>Already have an account?</p>
+                    <a href="">Sign In</a>
+                </div>
+                <p></p>
+            </div>
+            <h1>Free access 14 days trial</h1>
+            <div>
+                <input type="text" name="fullName" placeholder="Your Full Name" v-model="fullName">
+                <input type="text" name="userEmail" placeholder="Your Email Address" v-model="userEmail">
+                <input type="password" name="userPassword" placeholder="Your Password" v-model="userPassword">
+            </div>
+            <div>
+                <p>At least 8 characters</p>
+            </div>
+            <button type="button" @click="btnCreateAccount" >Create Account</button>
+        </div>
+        
+    </div>
 </template>
 
 <script>
 
 
 export default {
-  name: ,
-  components: {
+    name: 'App',
+    components: {},
+    data(){
+        return{
+            fullName: '',
+            userEmail: '',
+            userPassword: ''
 
-  }
+        }
+        
+
+    }
 }
 </script>
 
