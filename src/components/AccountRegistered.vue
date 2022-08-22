47f4f6d593cb48f7f4131c12f44750b028f477f1
Gwynshik <gwynshik@gmail.com>
2022-08-22 22:47:22 +0300

    added font for "Account registered"

--- src/components/AccountRegistered.vue
+++ src/components/AccountRegistered.vue
@@ -5,14 +5,18 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+
     .container-text{
         display: flex;
         height: 100%;
         width:  100%;
         align-items: center;
         justify-content: center;
+        
     }
     .container-text h1{
+        font-family: Lato;
         font-size: 36px;
         color: #242331;
     }
